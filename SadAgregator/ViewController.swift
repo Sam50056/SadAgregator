80d659ce35fb7721b357cbabb660f50ba2c35aec
Sam <sam912@mail.ru>
2020-11-11 14:50:32 +0400

    Initial Commit

---
+++ SadAgregator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SadAgregator
+//
+//  Created by Sam Yerznkyan on 11.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
