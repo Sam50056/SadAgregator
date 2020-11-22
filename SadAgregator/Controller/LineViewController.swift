465ba9ad42665fe2d16fd518c86c639f3b1763ba
Sam <sam912@mail.ru>
2020-11-22 21:41:59 +0400

    Add posts to lineVC

--- SadAgregator/Controller/LineViewController.swift
+++ SadAgregator/Controller/LineViewController.swift
@@ -20,8 +20,73 @@ class LineViewController: UIViewController {
     lazy var activityLineDataManager = ActivityLineDataManager()
     
     var lineData : JSON?
+    var postsArray = [JSON]()
     
     var activityTochkaCellsArray = [JSON]()
+    
+    var sizes : Array<[String]> {
+        get{
+            var thisArray = Array<[String]>()
+            
+            for post in postsArray {
+                
+                let sizesForThisPost = post["sizes"].arrayValue
+                
+                var stringSizesForThisPost = [String]()
+                
+                for size in sizesForThisPost {
+                    stringSizesForThisPost.append(size.stringValue)
+                }
+                
+                thisArray.append(stringSizesForThisPost)
+            }
+            
+            return thisArray
+        }
+    }
+    
+    var options : Array<[String]> {
+        get{
+            var thisArray = Array<[String]>()
+            
+            for post in postsArray {
+                
+                let optionsForThisPost = post["options"].arrayValue
+                
+                var stringOptionsForThisPost = [String]()
+                
+                for option in optionsForThisPost {
+                    stringOptionsForThisPost.append(option.stringValue)
+                }
+                
+                thisArray.append(stringOptionsForThisPost)
+            }
+            
+            return thisArray
+        }
+    }
+    
+    var images : Array<[String]> {
+        get{
+            var thisArray = Array<[String]>()
+            
+            for post in postsArray {
+                
+                let imagesForThisPost = post["images"].arrayValue
+                
+                var stringImagesForThisPost = [String]()
+                
+                for image in imagesForThisPost {
+                    stringImagesForThisPost.append(image["img"].stringValue)
+                }
+                
+                thisArray.append(stringImagesForThisPost)
+            }
+            
+            return thisArray
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +96,8 @@ class LineViewController: UIViewController {
         tableView.separatorStyle = .none
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.register(UINib(nibName: "PostTableViewCell", bundle: nil), forCellReuseIdentifier: "postCell")
         
         //        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
         refreshControl.addTarget(self, action: #selector(self.refresh(_:)), for: .valueChanged)
@@ -58,6 +125,8 @@ extension LineViewController : ActivityLineDataManagerDelegate{
             
             self.activityTochkaCellsArray = data["points_top"].arrayValue
             
+            self.postsArray = data["posts"].arrayValue
+            
             self.tableView.reloadData()
             
             self.refreshControl.endRefreshing()
@@ -84,7 +153,7 @@ extension LineViewController : ActivityLineDataManagerDelegate{
 extension LineViewController : UITableViewDelegate , UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3 + activityTochkaCellsArray.count + 1
+        return 3 + activityTochkaCellsArray.count + 1 + postsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -96,6 +165,8 @@ extension LineViewController : UITableViewDelegate , UITableViewDataSource{
         guard let lineData = lineData else {return cell}
         
         let maxIndexForActivityTochkaCells = 3 + activityTochkaCellsArray.count - 1 ///Max index (indexPath.row) for Activity tochka cells. So that 3 is because we have 3 static cells , then an array of activity tochka cells which is not static. So we do 3 + count of the array -1 (because array indexing starts from 0)  and get the max index we can put into switch. And what we'll put there will be 3..<array.count-1 This means that from 3rd index to 3 + array.count-1  all the cells will be "activityTochkaCell".
+        
+        let maxIndexForPosts = maxIndexForActivityTochkaCells + 1 + postsArray.count
         
         switch index {
         
@@ -129,6 +200,16 @@ extension LineViewController : UITableViewDelegate , UITableViewDataSource{
             
             cell = tableView.dequeueReusableCell(withIdentifier: "lastPostsCell", for: indexPath)
             
+        case maxIndexForActivityTochkaCells + 2...maxIndexForPosts:
+            
+            cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
+            
+            let index = indexPath.row - (maxIndexForActivityTochkaCells + 2)
+            
+            let post = postsArray[index]
+            
+            setUpPostCell(cell: cell as! PostTableViewCell, data: post, index: index)
+            
         default:
             print("IndexPath out of switch: \(index)")
         }
@@ -139,8 +220,24 @@ extension LineViewController : UITableViewDelegate , UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
+        let maxIndexForActivityTochkaCells = 3 + activityTochkaCellsArray.count - 1
+        let maxIndexForPosts = maxIndexForActivityTochkaCells + 1 + postsArray.count
+        
         if indexPath.row == 1 {
             return 126
+        }else if indexPath.row >= maxIndexForActivityTochkaCells + 2 && indexPath.row <= maxIndexForPosts{
+            
+            let index = indexPath.row - (maxIndexForActivityTochkaCells + 2)
+            
+            if options[index].count > 4{
+                return 500
+            }
+            
+            if options.count > 6 {
+                return 560
+            }
+            
+            return 460
         }
         
         return 50
@@ -149,7 +246,6 @@ extension LineViewController : UITableViewDelegate , UITableViewDataSource{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
     
     func setUpSwitchCell(cell : UITableViewCell, data: JSON){
         
@@ -197,5 +293,24 @@ extension LineViewController : UITableViewDelegate , UITableViewDataSource{
         }
         
     }
+    
+    func setUpPostCell(cell: PostTableViewCell , data : JSON, index : Int){
+        
+        cell.vendorLabel.text = data["vendor_capt"].stringValue
+        
+        cell.byLabel.text = data["by"].stringValue
+        
+        cell.priceLabel.text = "\(data["price"].stringValue) руб"
+        
+        let sizesArray = sizes[index]
+        let optionsArray = options[index]
+        let imagesArray = images[index]
+        
+        cell.sizes = sizesArray
+        cell.options = optionsArray
+        cell.images = imagesArray
+        
+    }
+    
     
 }
