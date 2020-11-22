05460d1f15fd9b434c1830fa90d45bbe79b89366
Sam <sam912@mail.ru>
2020-11-23 03:15:46 +0400

    Add tochka cell

---
+++ SadAgregator/Controller/TochkaViewController.swift
@@ -0,0 +1,89 @@
+//
+//  TochkaViewController.swift
+//  SadAgregator
+//
+//  Created by Sam Yerznkyan on 23.11.2020.
+//
+
+import UIKit
+import SwiftyJSON
+
+class TochkaViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tochkaData : JSON?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.separatorStyle = .none
+    }
+    
+}
+
+//MARK: - UITableView
+
+extension TochkaViewController : UITableViewDelegate , UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let index = indexPath.row
+        
+        var cell = UITableViewCell()
+        
+        guard let tochkaData = tochkaData else {return cell}
+        
+        switch index {
+        
+        case 0:
+            
+            cell = tableView.dequeueReusableCell(withIdentifier: "lineSwitchCell", for: indexPath)
+            
+        case 1:
+            
+            cell = tableView.dequeueReusableCell(withIdentifier: "generalPostsPhotosCell", for: indexPath)
+            
+            setUpGeneralPostsPhotosCell(cell: cell, data: tochkaData["activity"])
+            
+        default:
+            print("IndexPath out of switch: \(index)")
+        }
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.row == 0 ? 50 : 126
+    }
+    
+    //MARK: - Cell Setup
+    
+    func setUpGeneralPostsPhotosCell(cell : UITableViewCell , data : JSON){
+        
+        if let todaysPostsLabel = cell.viewWithTag(1) as? UILabel ,
+           let yesterdayPostsLabel = cell.viewWithTag(2) as? UILabel,
+           let todayPhotosLabel = cell.viewWithTag(3) as? UILabel,
+           let yesterdayPhotosLabel = cell.viewWithTag(4) as? UILabel {
+            
+            todaysPostsLabel.text = data["post_today"].stringValue
+            
+            yesterdayPostsLabel.text = data["post_ystd"].stringValue
+            
+            todayPhotosLabel.text = data["photo_today"].stringValue
+            
+            yesterdayPhotosLabel.text = data["photo_ystd"].stringValue
+            
+        }
+        
+    }
+    
+}
