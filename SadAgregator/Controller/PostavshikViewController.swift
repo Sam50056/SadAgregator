d32b14299156db587b1dd406d697992410ab1b40
Sam <sam912@mail.ru>
2020-12-07 23:44:23 +0400

    Add posts

--- SadAgregator/Controller/PostavshikViewController.swift
+++ SadAgregator/Controller/PostavshikViewController.swift
@@ -22,6 +22,69 @@ class PostavshikViewController: UIViewController {
     
     var vendorData : JSON?
     
+    var sizes : Array<[String]> {
+        get{
+            var thisArray = Array<[String]>()
+            
+            for post in postsArray {
+                
+                let sizesForThisPost = post["sizes"].arrayValue
+                
+                var stringSizesForThisPost = [String]()
+                
+                for size in sizesForThisPost {
+                    stringSizesForThisPost.append(size.stringValue)
+                }
+                
+                thisArray.append(stringSizesForThisPost)
+            }
+            
+            return thisArray
+        }
+    }
+    
+    var options : Array<[String]> {
+        get{
+            var thisArray = Array<[String]>()
+            
+            for post in postsArray {
+                
+                let optionsForThisPost = post["options"].arrayValue
+                
+                var stringOptionsForThisPost = [String]()
+                
+                for option in optionsForThisPost {
+                    stringOptionsForThisPost.append(option.stringValue)
+                }
+                
+                thisArray.append(stringOptionsForThisPost)
+            }
+            
+            return thisArray
+        }
+    }
+    
+    var images : Array<[String]> {
+        get{
+            var thisArray = Array<[String]>()
+            
+            for post in postsArray {
+                
+                let imagesForThisPost = post["images"].arrayValue
+                
+                var stringImagesForThisPost = [String]()
+                
+                for image in imagesForThisPost {
+                    stringImagesForThisPost.append(image["img"].stringValue)
+                }
+                
+                thisArray.append(stringImagesForThisPost)
+            }
+            
+            return thisArray
+        }
+    }
+    
     var vendorPhone : String? {
         return vendorData?["phone"].stringValue
     }
@@ -57,6 +120,8 @@ class PostavshikViewController: UIViewController {
         tableView.dataSource = self
         
         tableView.separatorStyle = .none
+        
+        tableView.register(UINib(nibName: "PostTableViewCell", bundle: nil), forCellReuseIdentifier: "postCell")
         
         if let safeId = thisVendorId{
             vendorCardDataManager.getVendorCardData(key: key, vendorId: safeId)
@@ -95,11 +160,11 @@ extension PostavshikViewController : VendorCardDataManagerDelegate{
 extension PostavshikViewController : UITableViewDelegate , UITableViewDataSource{
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 5
+        return 6
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      
+        
         switch section {
         
         case 0:
@@ -119,6 +184,10 @@ extension PostavshikViewController : UITableViewDelegate , UITableViewDataSource
             return vendorRevs.count
             
         case 4:
+            
+            return 1
+            
+        case 5:
             
             return postsArray.count
             
@@ -163,6 +232,21 @@ extension PostavshikViewController : UITableViewDelegate , UITableViewDataSource
                 cell = tableView.dequeueReusableCell(withIdentifier: "revCountLabel", for: indexPath)
             }
             
+        case 4:
+            
+            cell = tableView.dequeueReusableCell(withIdentifier: "lastPostsCell", for: indexPath)
+            
+        case 5:
+            
+            cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
+            
+            let index = indexPath.row
+            
+            let post = postsArray[index]
+            
+            setUpPostCell(cell: cell as! PostTableViewCell, data: post, index: index)
+            
+            
         default:
             print("Index Path Section out of switch : \(indexPath.section)")
         }
@@ -170,9 +254,56 @@ extension PostavshikViewController : UITableViewDelegate , UITableViewDataSource
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        switch indexPath.section {
+        
+        case 0:
+            
+            return 92
+            
+        case 1:
+            
+            return 50
+            
+        case 2:
+            
+            return 50
+            
+        case 3:
+            
+            return 50
+            
+        case 4:
+            
+            return 50
+            
+        case 5:
+            
+            let index = indexPath.row
+            
+            if options[index].count > 4{
+                return 500
+            }
+            
+            if options.count > 6 {
+                return 560
+            }
+            
+            return 460
+            
+            
+        default:
+            fatalError("Invalid Section")
+        }
+        
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK: - Row Count Stuff
     
     func getInfoRowsCount() -> Int{
         
@@ -315,6 +446,26 @@ extension PostavshikViewController : UITableViewDelegate , UITableViewDataSource
             thisInfoCellObject.shouldRightLabelBeBlue ? (rightLabel.textColor = .systemBlue) : (rightLabel.textColor = #colorLiteral(red: 0.3666185141, green: 0.3666757345, blue: 0.3666060269, alpha: 1))
             
         }
+        
+    }
+    
+    func setUpPostCell(cell: PostTableViewCell , data : JSON, index : Int){
+        
+        cell.vendorLabel.text = data["vendor_capt"].stringValue
+        
+        cell.byLabel.text = data["by"].stringValue
+        
+        cell.priceLabel.text = "\(data["price"].stringValue) руб"
+        
+        cell.postedLabel.text = data["posted"].stringValue
+        
+        let sizesArray = sizes[index]
+        let optionsArray = options[index]
+        let imagesArray = images[index]
+        
+        cell.sizes = sizesArray
+        cell.options = optionsArray
+        cell.images = imagesArray
         
     }
     
