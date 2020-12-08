5c02b47fe6fa7be3cadada280d227f216b8e3c6e
Sam <sam912@mail.ru>
2020-12-08 17:00:56 +0400

    Delete it )

--- SadAgregator/Controller/MainViewController.swift
+++ SadAgregator/Controller/MainViewController.swift
@@ -132,13 +132,6 @@ class MainViewController: UIViewController {
         
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        refresh(self)
-        
-    }
-    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
