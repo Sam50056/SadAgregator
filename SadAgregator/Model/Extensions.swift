c9d11215238b0fe1f24e219427f2965ad62ca924
Sam <sam912@mail.ru>
2020-12-17 20:09:27 +0400

    Clear Extensions file , no need for that

--- SadAgregator/Model/Extensions.swift
+++ SadAgregator/Model/Extensions.swift
@@ -6,20 +6,3 @@
 //
 
 import Foundation
-import MobileCoreServices
-
-extension URL {
-    func mimeType() -> String {
-        
-        let pathExtension = self.pathExtension
-        
-        if let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension as NSString, nil)?.takeRetainedValue() {
-            if let mimetype = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType)?.takeRetainedValue() {
-                return mimetype as String
-            }
-        }
-        return "application/octet-stream"
-        
-    }
-    
-}
