558ab44d82460dcea1af5de4be257313bf8f6a2e
Sam <sam912@mail.ru>
2020-12-26 21:32:37 +0400

    Design update ListSelectView

--- SadAgregator/View/SwiftUiViews/Master Views /ListSelectView.swift
+++ SadAgregator/View/SwiftUiViews/Master Views /ListSelectView.swift
@@ -15,93 +15,85 @@ struct ListSelectView: View {
     
     var body: some View {
         
-        GeometryReader { geometry in
+        VStack(alignment: .leading, spacing: 16){
             
-            ScrollView{
+            VStack(alignment: .leading,spacing: 16){
                 
-                VStack(alignment: .leading, spacing: 16){
+                Text(masterViewModel.currentViewData!["capt"].stringValue)
+                    .font(.system(size: 21))
+                    .bold()
+                
+                if masterViewModel.currentViewData!["hint"].stringValue != "" {
                     
-                    VStack(alignment: .leading,spacing: 16){
-                        
-                        Text(masterViewModel.currentViewData!["capt"].stringValue)
-                            .font(.system(size: 21))
-                            .bold()
-                        
-                        if masterViewModel.currentViewData!["hint"].stringValue != "" {
-                            
-                            Text(masterViewModel.currentViewData!["hint"].stringValue)
-                                .foregroundColor(Color(.systemGray))
-                                .font(.system(size: 17))
-                            
-                        }
-                        
-                        HStack{
-                            
-                            Image(systemName: "magnifyingglass")
-                                .resizable()
-                                .frame(width: 20, height: 20, alignment: .center)
-                                .foregroundColor(Color(.systemGray))
-                            
-                            TextField("Поиск по списку", text: $text)
-                            
-                        }
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color(.white))
-                        .cornerRadius(8)
-                        .shadow(radius: 4)
-                        
-                    }
+                    Text(masterViewModel.currentViewData!["hint"].stringValue)
+                        .foregroundColor(Color(.systemGray))
+                        .font(.system(size: 17))
                     
+                }
+                
+                HStack{
+                    
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .frame(width: 20, height: 20, alignment: .center)
+                        .foregroundColor(Color(.systemGray))
+                    
+                    TextField("Поиск по списку", text: $text)
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color(.white))
+                .cornerRadius(8)
+                .shadow(radius: 4)
+                
+            }
+            
+            VStack{
+                
+                ForEach(masterViewModel.items , id: \.id){ item in
                     
                     VStack{
                         
-                        ForEach(masterViewModel.items , id: \.id){ item in
+                        HStack{
                             
-                            VStack{
+                            VStack(alignment: .leading, spacing: 8){
                                 
-                                HStack{
-                                    
-                                    VStack(alignment: .leading, spacing: 8){
-                                        
-                                        Text(item.capt)
-                                        
-                                        if item.hint != ""{
-                                            Text(item.hint)
-                                                .foregroundColor(Color(.systemGray))
-                                        }
-                                        
-                                    }
-                                    .font(.system(size: 16))
-                                    
-                                    Spacer()
-                                    
-                                    if item.button != ""{
-                                        Text("Connected")
-                                            .foregroundColor(Color(.systemBlue))
-                                    }
-                                    
+                                Text(item.capt)
+                                
+                                if item.hint != ""{
+                                    Text(item.hint)
+                                        .foregroundColor(Color(.systemGray))
                                 }
                                 
-                                Divider()
-                                
                             }
-                            .onTapGesture {
-                                print("Selected item: \(item)")
+                            .font(.system(size: 16))
+                            
+                            Spacer()
+                            
+                            if item.button != ""{
+                                Text("Connected")
+                                    .foregroundColor(Color(.systemBlue))
                             }
                             
                         }
                         
+                        Divider()
+                        
                     }
-                    .padding()
+                    .onTapGesture {
+                        print("Selected item: \(item)")
+                    }
                     
                 }
-                .padding(.horizontal,16)
-                .frame(width: geometry.size.width) // Make the scroll view full-width
-                .frame(minHeight: geometry.size.height) // Set the content’s min height to the parent
                 
             }
+            .padding()
+            .background(Color(.white))
+            .cornerRadius(12)
+            
         }
+        .padding(.horizontal,16)
         
     }
     
