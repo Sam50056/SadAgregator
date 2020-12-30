39d7a021b4e848413fd46a4b11b27468de9a50a9
Sam <sam912@mail.ru>
2020-12-30 04:23:03 +0400

    Add AddPointRequestView

---
+++ SadAgregator/View/SwiftUiViews/Menu Views/AddPointRequestView.swift
@@ -0,0 +1,88 @@
+//
+//  AddPointRequestView.swift
+//  SadAgregator
+//
+//  Created by Sam Yerznkyan on 30.12.2020.
+//
+
+import SwiftUI
+
+struct AddPointRequestView: View {
+    
+    @State var text1 = ""
+    @State var text2 = ""
+    @State var text3 = ""
+    
+    var body: some View {
+        
+        ScrollView{
+            
+            VStack{
+                
+                VStack(spacing: 32){
+                    
+                    AddPointRequestViewSubView(text: $text1, placeholderText: "Номер места")
+                    
+                    AddPointRequestViewSubView(text: $text2, placeholderText: "Ссылка на поставщика в vk.com")
+                    
+                    AddPointRequestViewSubView(text: $text3, placeholderText: "Комментарий")
+                    
+                }
+                
+                Spacer()
+                    .frame(height: 16)
+                
+                HStack{
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }){
+                        
+                        Text("Добавить ссылку")
+                            .padding(10)
+                            .background(Color(.systemBlue))
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                        
+                    }
+                    
+                }
+                
+            }
+            .padding()
+            
+        }
+        .navigationBarTitle(Text("Садовод - Агрегатор"), displayMode : .inline)
+        
+    }
+    
+}
+
+struct AddPointRequestView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPointRequestView()
+    }
+}
+
+struct AddPointRequestViewSubView : View{
+    
+    @Binding var text : String
+    
+    var placeholderText : String
+    
+    var body: some View{
+        
+        VStack{
+            
+            TextField(placeholderText, text: $text)
+            
+            Divider()
+            
+        }
+        
+    }
+    
+}
+
