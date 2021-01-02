77f16a3ec09b134b5f0314d7c7cd063d0b7ab289
Sam <sam912@mail.ru>
2021-01-02 06:41:23 +0400

    Add AuthSocialDataManager

---
+++ SadAgregator/Model/Managers/Auth Managers/AuthSocialDataManager.swift
@@ -0,0 +1,50 @@
+//
+//  AuthSocialDataManager.swift
+//  SadAgregator
+//
+//  Created by Sam Yerznkyan on 02.01.2021.
+//
+
+import Foundation
+import SwiftyJSON
+
+protocol AuthSocialDataManagerDelegate {
+    func didGetAuthSocialData(data : JSON)
+    func didFailGettingAuthSocialDataWithError(error : String)
+}
+
+struct AuthSocialDataManager {
+    
+    var delegate : AuthSocialDataManagerDelegate?
+    
+    func getGetAuthSocialData(social : String ,token : String){
+        
+        let urlString = "https://tk-sad.ru/i/baza/auth-social?social=\(social)&token=\(token)"
+        
+        print("URLString for AuthSocialDataManager: \(urlString)")
+        
+        guard let encodedURL = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let url = URL(string: encodedURL)  else {return}
+        
+        let session = URLSession(configuration: .default)
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil {
+                delegate?.didFailGettingAuthSocialDataWithError(error: error!.localizedDescription)
+                return
+            }
+            
+            guard let data = data else {delegate?.didFailGettingAuthSocialDataWithError(error: "Data is empty"); return}
+            
+            let json = String(data: data , encoding: String.Encoding.windowsCP1251)!
+            
+            let jsonAnswer = JSON(parseJSON: json)
+            
+            delegate?.didGetAuthSocialData(data: jsonAnswer)
+        }
+        
+        task.resume()
+        
+    }
+    
+}
