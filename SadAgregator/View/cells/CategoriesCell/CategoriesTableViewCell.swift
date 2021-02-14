032d13dc6b6f183e7a030d2bdcad8b4be76966b2
Sam <sam912@mail.ru>
2021-02-15 02:14:51 +0400

    Add "См. все Категории" cell

--- SadAgregator/View/cells/CategoriesCell/CategoriesTableViewCell.swift
+++ SadAgregator/View/cells/CategoriesCell/CategoriesTableViewCell.swift
@@ -36,20 +36,29 @@ class CategoriesTableViewCell: UITableViewCell {
 extension CategoriesTableViewCell : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return categories.count
+        return categories.count == 0 ? 0 : categories.count + 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "categoryCell", for: indexPath) as! CategoryCollectionViewCell
+        
+        cell.view.layer.cornerRadius = 8
+        
+        //This is an additional cell
+        if indexPath.row == 0{
+            
+            cell.firstLabel.text = "См. все"
+            cell.secondLabel.text = "Категории"
+            
+            return cell
+        }
        
-        let category = categories[indexPath.row]
+        let category = categories[indexPath.row - 1] //We do -1 because we added one more cell above 
         
         cell.firstLabel.text = category["capt"].stringValue
         
         cell.secondLabel.text = category["descr"].stringValue
-        
-        cell.view.layer.cornerRadius = 8
         
         return cell
         
@@ -58,6 +67,5 @@ extension CategoriesTableViewCell : UICollectionViewDelegate, UICollectionViewDa
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 0, left: 16, bottom: 16, right: 16)
     }
-    
     
 }
