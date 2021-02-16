54f50aa9985fc70b95b5af81eba0d1aa32c317b0
Sam <sam912@mail.ru>
2021-02-17 01:09:58 +0400

    Add an extension to view for rounding the corners

--- SadAgregator/Model/Supporting Files /Extensions.swift
+++ SadAgregator/Model/Supporting Files /Extensions.swift
@@ -162,3 +162,19 @@ extension UIViewController {
     }
     
 }
+
+//MARK: - UIView
+
+extension UIView {
+    
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+    
+}
+
+
