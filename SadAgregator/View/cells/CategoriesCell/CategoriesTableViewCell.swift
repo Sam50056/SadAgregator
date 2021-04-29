e7f345141d0cb1e87a7cb8091fa99bc32c63e275
Sam <sam912@mail.ru>
2021-04-29 15:58:23 +0400

    Fix categories

--- SadAgregator/View/cells/CategoriesCell/CategoriesTableViewCell.swift
+++ SadAgregator/View/cells/CategoriesCell/CategoriesTableViewCell.swift
@@ -24,6 +24,8 @@ class CategoriesTableViewCell: UITableViewCell {
         collectionView.delegate = self
         collectionView.dataSource = self
         
+        collectionView.collectionViewLayout = createLayout()
+        
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -35,7 +37,39 @@ class CategoriesTableViewCell: UITableViewCell {
 
 //MARK: - UICollectionView
 
-extension CategoriesTableViewCell : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+extension CategoriesTableViewCell : UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func createLayout() -> UICollectionViewLayout {
+        
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, NSCollectionViewLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            
+            let section: NSCollectionLayoutSection
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .estimated(100), heightDimension: .fractionalHeight(1))
+            
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 0.5, leading: 0.5, bottom: 0.5, trailing: 0.5)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(100), heightDimension: .absolute(60))
+            
+            let group =  NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            
+            group.interItemSpacing = .fixed(10)
+            
+            section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = 16
+            section.orthogonalScrollingBehavior = .continuous
+            
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 5, trailing: 0)
+            
+            return section
+            
+        }
+        
+        return layout
+        
+    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return categories.count == 0 ? 0 : categories.count + 1
@@ -73,8 +107,8 @@ extension CategoriesTableViewCell : UICollectionViewDelegate, UICollectionViewDa
         categoryCellTapped?(categories[indexPath.row - 1]["id"].stringValue)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 16, bottom: 16, right: 16)
-    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 0, left: 16, bottom: 16, right: 16)
+//    }
     
 }
