fbc1ad09ac958cd256c723513448217c56a0969e
Sam <sam912@mail.ru>
2021-08-22 04:56:34 +0400

    check sending after balance change for client

---
+++ SadAgregator/Model/Managers/Clients Managers/Clients/Payment/ClientsUpdatePayImageDataManager.swift
@@ -0,0 +1,44 @@
+//
+//  ClientsUpdatePayImageDataManager.swift
+//  SadAgregator
+//
+//  Created by Sam Yerznkyan on 22.08.2021.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct ClientsUpdatePayImageDataManager {
+    
+    func getClientsUpdatePayImageData(key : String , payHistId : String , imgId : String , completionHandler: @escaping (JSON?, String?) -> Void){
+        
+        let urlString = "https://agrapi.tk-sad.ru/agr_clients.UpdatePayImg?AKey=\(key)&APayHistID=\(payHistId)&AImgID=\(imgId)"
+        
+        print("URLString for ClientsUpdatePayImageDataManager: \(urlString)")
+        
+        guard let encodedURL = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let url = URL(string: encodedURL)  else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            //            print(String(data: data!, encoding: String.Encoding.windowsCP1251)!)
+            
+            if error != nil {
+                completionHandler(nil , error?.localizedDescription)
+                return
+            }
+            
+            guard let data = data else {completionHandler(nil ,"Data is empty");  return}
+            
+            let json = String(data: data , encoding: String.Encoding.windowsCP1251)!
+            
+            let jsonAnswer = JSON(parseJSON: json)
+            
+            completionHandler(jsonAnswer , nil)
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+}
