5954c89aa2b463a95cc6e9a805e1986b430512aa
Sam <sam912@mail.ru>
2022-03-06 15:04:23 +0400

    bring tab/nav bars back to iOS 14 look and feel

--- SadAgregator/AppDelegate.swift
+++ SadAgregator/AppDelegate.swift
@@ -18,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        if #available(iOS 15, *) {
+            UINavigationBar.appearance().scrollEdgeAppearance = UINavigationBarAppearance()
+            UITabBar.appearance().scrollEdgeAppearance = UITabBarAppearance()
+        }
+        
         configureNotification(application: application)
         
         IQKeyboardManager.shared.enable = true
